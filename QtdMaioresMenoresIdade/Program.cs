07e85b66d902b9400c1c13a49b75885c1d990000
veronezzi159 <lucasveronezzi3@gmail.com>
2024-04-19 15:51:42 -0300

    programa

--- QtdMaioresMenoresIdade/Program.cs
+++ QtdMaioresMenoresIdade/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿int idade = 0, count = 0, qtdmaiores = 0, qtdmenores = 0;
+
+
+do
+{
+    Console.WriteLine($"Escreva a idade da pessoa {count + 1}");
+    idade = int.Parse( Console.ReadLine() );
+
+    if ( idade < 1 )
+    {
+        Console.WriteLine("idade zerada impossivel");
+    }
+    else
+    {
+        if (idade > 17)
+        {
+            qtdmaiores++;            
+        } else
+        {
+            qtdmenores++;
+        }
+        count++;
+    }
+
+} while (count <10);
+
+Console.WriteLine($"O numero de pessoas maiores de idade é: {qtdmaiores}");
+Console.WriteLine($"O numero de pessoas menores de idade é: {qtdmenores}");
